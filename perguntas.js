4117e98aa9cbd76dd7d8651db88c5bd6bb26a874
caio11112 <caionacarini@hotmail.com>
2024-11-29 15:27:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'red dead redeption 2 ',
+    'como arthur morgan morre?',
+    'Na primeira escolha, ele logo é emboscado pelo vingativo Micah, e Dutch intervém. Arthur convence Dutch a abandonar Micah e ir embora. Se o jogador tiver grande honra, Arthur sucumbe aos seus ferimentos e doença, morrendo pacificamente enquanto observa o nascer do sol; com pouca honra, Micah o executa.'
+)
+
+criaCartao(
+    'red dead redeption 2',
+    'Quem foi arthur morgan?',
+    'Arthur Morgan é um personagem fictício e o protagonista de Red Dead Redemption 212. Ele é um membro da gangue Van der Linde e deve lidar com o declínio do Velho Oeste enquanto tenta sobreviver contra as forças do governo1. Arthur nasceu por volta de 1863 no norte dos EUA, filho de Beatrice e Lyle Morgan, um pequeno criminoso e fora-da-lei2. Ele é interpretado por Roger Clark1'
+)
+
+criaCartao(
+    'red dead redeption 2 ',
+    'em qual epoca se passa a historia do jogo',
+    '1899'
+)
+
+criaCartao(
+    'red dead redeption 2',
+    'artur morgan se redimiu?',
+    'Ao longo do jogo, Arthur se esforça para se redimir, buscando fazer o que é certo, mesmo que isso signifique confrontar seu próprio passado.'
+)
